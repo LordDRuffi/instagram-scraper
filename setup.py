2e54d274634793d52d9b87dd7856811b62d244c2
LordDRuffi <dominik-childs@t-online.de>
2020-03-01 23:09:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     name="igramscraper",
     version="0.3.2",
     description=('scrapes medias, likes, followers, tags and all metadata'),
-    long_description=Path("README.md").read_text(),
+    long_description="No Description",
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     license="MIT",
